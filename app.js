168171caf4593c91a091aea0ff564cd40b1073d8
Leandro Strifezza <leandro.strifezza@xyclon.com.ar>
2024-05-14 17:31:32 -0300

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('hola')
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+let buttonInstall = document.getElementById('install');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+//   showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+  
+});
+
+buttonInstall.addEventListener('click', async () => {
+    console.log('hola')
+    // Hide the app provided install promotion
+    // hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt and can't use it again, throw it away
+    deferredPrompt = null;
+  });
